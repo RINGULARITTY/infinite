0804caac6dd25254754ba8313e5fd8bfb8a28af1
RINGULARITTY <63746619+RINGULARITTY@users.noreply.github.com>
2023-08-24 16:30:06 +0200

    Base mod launching

---
+++ src/main/java/ringularity/infinite/InfiniteDataGenerator.java
@@ -0,0 +1,11 @@
+package ringularity.infinite;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class InfiniteDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
